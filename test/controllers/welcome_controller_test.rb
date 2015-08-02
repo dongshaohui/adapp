c07782916e9de9e59ef9eb45e119961bee1a8dc8
dongshaohui <dongsh@jsfund.cn>
2015-08-03 02:03:34 +0800

    提交广点通插屏广告

--- test/controllers/welcome_controller_test.rb
+++ test/controllers/welcome_controller_test.rb
@@ -1,8 +1,8 @@
 require 'test_helper'
 
 class WelcomeControllerTest < ActionController::TestCase
-  test "should get index" do
-    get :index
+  test "should get ads" do
+    get :ads
     assert_response :success
   end
 
